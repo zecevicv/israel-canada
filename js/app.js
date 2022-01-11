6908d171e4cdcf9522595ddb40a457ffa3fa4c79
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2022-01-11 10:52:03 +0100

    [ADD] - ICR Page

--- js/app.js
+++ js/app.js
@@ -19,3 +19,29 @@ if (header) {
     header.classList.remove('header-scroll');
   }
 }
+
+/* #Projects Slider
+================================================== */
+if (document.querySelector(".projects-slider .swiper")) {
+  new Swiper(".projects-slider .swiper", {
+    navigation: {
+      nextEl: ".projects-slider .next",
+      prevEl: ".projects-slider .prev",
+    },
+    pagination: {
+      el: ".projects-slider .swiper-pagination",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1.36,
+        loop: true,
+        centeredSlides: true,
+      },
+      1024: {
+        slidesPerView: 3.25,
+        loop: false,
+        centeredSlides: false,
+      }
+    }
+  });
+}
